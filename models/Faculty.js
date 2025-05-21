9410c546521ee340375ee959b2001b1c198a0123
Tekisky123 <153596067+Tekisky123@users.noreply.github.com>
2025-05-21 16:27:25 +0530

    update

--- models/Faculty.js
+++ models/Faculty.js
@@ -7,7 +7,7 @@ const FacultySchema = new mongoose.Schema({
     designation: { type: String, required: true },
     dateOfJoining: { type: String },
     qualification: { type: String, required: true },
-    experience: { type: String, required: true },
+    experience: { type: String },
     photo: { type: String,  default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" },
 });
 
